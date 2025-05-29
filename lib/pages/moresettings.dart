6b9936f65c52e306602471cd922aaacee0d02ce9
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-05-29 12:02:24 -0500

    Help and Feedback works

--- lib/pages/moresettings.dart
+++ lib/pages/moresettings.dart
@@ -102,6 +102,67 @@ void initState() {
   );
 }
 
+
+ void showHelpBottomSheet(BuildContext context) {
+  final TextEditingController reportController = TextEditingController();
+
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    builder: (BuildContext context) {
+      return Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+          left: 16,
+          right: 16,
+          top: 20,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Help & Feedback',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "Please tell us your question:",
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: reportController,
+              maxLines: 4,
+              decoration: const InputDecoration(
+                hintText: 'Enter your question here...',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: () async {
+                  final reportText = reportController.text.trim();
+                  if (reportText.isNotEmpty) {
+                    await databaseRef.child('help').push().set({
+      'text': reportText, 'reporter' : _currentUsername
+      
+    });
+                    Navigator.pop(context);
+                  }
+                },
+                child: const Text('Send'),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      );
+    },
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -142,7 +203,7 @@ void initState() {
             leading: const Icon(Icons.help_outline, color: Colors.white),
             title: const Text('Help & Feedback', style: TextStyle(color: Colors.white)),
             onTap: () {
-              // Add navigation or logic here
+              showHelpBottomSheet(context);
             },
           ),
 
